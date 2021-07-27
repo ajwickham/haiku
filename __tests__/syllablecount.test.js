9eb3e909ebf7a667ce17dd52c15a6b185a630ac6
Alan Wickham <ajwickham@yahoo.com>
2021-07-27 21:23:41 +0100

    Finds second vowel

--- __tests__/syllablecount.test.js
+++ __tests__/syllablecount.test.js
@@ -11,4 +11,22 @@ describe('Syllable Count', () => {
     expect(testWord.word).toEqual("ear");
     expect(testWord.syllables).toEqual(2);
   });
+  test('B.  Should correctly split syllable object into letter array', () => {
+    expect((testWord.word).split("")).toEqual(["e","a","r"]);  
+  });
+  test('C.  Should correctly find the next vowel', () => {
+    testWord.findNextVowel();
+    expect(testWord.syllables).toEqual(1);  
+  });
+  let testWord1;
+  beforeEach(() => {
+    testWord1 = new Syllable("street",1);
+  })
+  test('D.  Should correctly find the next vowel', () => {
+    testWord1.findNextVowel();
+    expect(testWord1.syllables).toEqual(1);  
+  });
+  test('E.  Should correctly find the subsequent vowel', () => {
+    expect(testWord1.findNextVowel()).toEqual("yippee");  
+  });
 });
--- src/js/syllable.js
+++ src/js/syllable.js
@@ -4,8 +4,28 @@ export default class Syllable {
       this.syllables = syllables;
     }
     
-  /*getArea() {
-    return this.side1 * this.side2;
-    }*/
-
+  findNextVowel(){
+    let count =0;
+    let x;
+    for(let i = 0; i<((this.word).split("")).length; i++) {
+        x= ((this.word).split(""))[i]
+        if(/[aeiouy]/.test(x)) {
+          count = count + 1;
+          this.syllables = count;
+          
+          if(i<((this.word).split("")).length) {   //checks if at the end of the word
+            for(let j = i+1; j<(((this.word).split("")).length);j++) {
+                if(/[aeiouy]/.test(((this.word).split(""))[j]))
+                return "yippee" 
+            }  
+            /*
+            if(/e/.test(((this.word).split(""))[i+1])) { //ignores next letter if it is e
+                  i=i+1;
+              }
+              
+              if()*/
+          }
+      };
+    };    
+  }  
 }
